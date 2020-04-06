f25ea1fc802c278b971e80aa5b19e26e2b2a5939
whashi44 <whashi44@gmail.com>
2020-04-06 13:36:30 -0500

    Some file organizing

--- lindemann_index/calc_Li_final.py
+++ lindemann_index/calc_Li_final.py
@@ -7,59 +7,41 @@
     3. Read the content of teh file
     4. Extract atom coordinates
     5. Calculate the Lindemann Index
-
-
 """
 
 #standard library
 import os,sys,re,time
 import os.path as osp
-from importlib import import_module
 from pprint import pprint
 
-# test
-# from numba import jit
+# pip packages
+import numpy as np
+import natsort as nt
+from ovito.io import import_file
+from ovito.modifiers import SelectTypeModifier
+import tqdm
 
-def import_library():
-    named_libs = [('numpy','np'),('natsort','nt'),
-    ('ovito','ov'),('tqdm','tqdm')]
-    # input_flag = input("Would you like to import missing modules to your user folder? y/n: ")
-    input_flag = 'y'
-    import_flag = bool(input_flag=='y')
 
-    for name,short in named_libs:
-        try:
-            lib = import_module(name)
-        except ModuleNotFoundError | ImportError:
-            if import_flag==False:
-                print(sys.exc_info())
-            elif import_flag == True:
-                from pip._internal import main as pip
-                print(f'Installing {name} to your local user folder\n\n\n')
-                pip(['install','--user',f'{name}'])
-        else:
-            globals()[short] = lib
 
+# get the list of file in the directory that meet the criteria
 def get_filelist(file_extension='.lammpstrj'):
     file_list = []
-
+    file_prefix = 'nano3'
     print(f'Using {file_extension} as target file extension')
     all_directory = os.listdir()
 
     for file in all_directory:
         if osp.isfile(file) and file.endswith(file_extension):
-        # if osp.isfile(file) and file.endswith(file_extension) and file.startswith('prod'):
+        # if osp.isfile(file) and file.endswith(file_extension) and file.startswith(file_prefix):
             file_list.append(file)
     file_list = nt.natsorted(file_list)
     return file_list
 
 def get_numlist(file_list):
     num_list = []
-
     for file in file_list:
             num_list.append(re.findall(r'\d+', file)) #re for getting number from file name
     num_list = nt.natsorted(num_list)
-
     return num_list
 
 def calc_lindex():
@@ -79,8 +61,13 @@ def calc_lindex():
         t = time.time()
         print(f"Calculating the Lindemann Index for file {file}")
         #   importing the files
-        pipeline = ov.io.import_file(file, sort_particles=True)
+        pipeline = import_file(file, sort_particles=True)
         num_frame = pipeline.source.num_frames
+        pipeline.modifiers.append(SelectTypeModifier(
+            operate_on = "particles",
+            property = "Particle Type",
+            types = {1,2,3}
+        ))
         data = pipeline.compute()
         #   Initilizations
         num_particle = data.particles.count
@@ -118,7 +105,6 @@ def calc_lindex():
         print(f"Elapsed time is {calc_time}")
 # ----------Scan and Store file ----------------
 def main():
-    import_library()
     calc_lindex()
 #   Useful for debugging
 # np.savetxt('test.txt',position[:,:,0])
